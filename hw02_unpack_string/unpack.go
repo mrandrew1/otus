237d611e8b693a4ed71d19a3ee24526bc586f7c8
Andrew Gusev <a.gusev@dataduck.com>
2023-11-25 15:10:53 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
+
+const escape = '\\'
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var out strings.Builder
+	var c rune
+	var flagEscape bool
+
+	out.Grow(len(s))
+
+	for index, value := range s {
+		switch {
+		case value == escape && !flagEscape:
+			flagEscape = true
+		case flagEscape:
+			out.WriteRune(value)
+			flagEscape, c = false, value
+		case unicode.IsNumber(value):
+			if c != 0 {
+				for a := 1; a < int(value-'0'); a++ {
+					out.WriteRune(c)
+				}
+				c = 0
+			} else {
+				return "", errors.New("invalid string")
+			}
+		default:
+			if (len(s) > (index+1) && s[index+1] != '0') || (len(s) == (index + 1)) {
+				out.WriteRune(value)
+				c = value
+			}
+		}
+	}
+	return out.String(), nil
 }
